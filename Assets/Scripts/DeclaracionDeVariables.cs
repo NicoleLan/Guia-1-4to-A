46feea2ca6b04d4646f42a2a054c0fae97cad719
NicoleLan <167232993+NicoleLan@users.noreply.github.com>
2025-03-25 11:44:54 -0300

    Ejercicio 1 - Terminado

--- Assets/Scripts/DeclaracionDeVariables.cs
+++ Assets/Scripts/DeclaracionDeVariables.cs
@@ -12,7 +12,15 @@ public class DeclaracionDeVariables : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        cantidadAlumnos = 24;
+        magnitudTerremoto = 9.3f; // ponemos una "f" para re-indicarle a Visual que es un Float
+        nombreAlumno = "Nicole";
+        compuHabilitada = false;
+
+        Debug.Log(cantidadAlumnos); // Debug = console
+        Debug.Log(magnitudTerremoto);
+        Debug.Log(nombreAlumno);
+        Debug.Log(compuHabilitada);
     }
 
     // Update is called once per frame
