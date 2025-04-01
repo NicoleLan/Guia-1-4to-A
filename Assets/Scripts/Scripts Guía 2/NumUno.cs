65840868450e8e3c68f75e2d58aeb6fa0f71856d
NicoleLan <167232993+NicoleLan@users.noreply.github.com>
2025-04-01 12:12:36 -0300

    Guía 2 - Ejercicio 1 - Terminado (Unity & Visual)

---
+++ Assets/Scripts/Scripts Guía 2/NumUno.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumUno : MonoBehaviour
+{
+    int numUno = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        numUno += 2;
+        Debug.Log(numUno);
+        numUno *= numUno;
+        Debug.Log(numUno); // o Debug.Log (numUno *= numUno);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
