847b8a14109a09a3650f4a669cdc9919ef80f0f5
NicoleLan <167232993+NicoleLan@users.noreply.github.com>
2025-04-08 12:19:28 -0300

    Ejercicio: De que curso sos - Terminado

---
+++ Assets/Scripts/Scripts De que curso sos/DeQueCursoSos.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeQueCursoSos : MonoBehaviour
+{
+    public string studentName;
+    public int studentYear;
+    public string studentOrientation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if ((studentName == "") || (studentYear > 5) || (studentOrientation != "T" && studentOrientation != "D" && studentOrientation == "G" && studentOrientation != "M" && studentOrientation !="H" ))
+        {
+            Debug.Log("Por favor revise si es válida la información ingresada.");
+            return;
+        }else if (studentYear <3 && studentOrientation != "")
+        {
+            Debug.Log("Error, el alumno pertenece a ciclo básico, no puede tener orientación.");
+            return;
+        }else
+        {
+            Debug.Log("Muchas gracias " + studentName);
+        }
+    } 
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
